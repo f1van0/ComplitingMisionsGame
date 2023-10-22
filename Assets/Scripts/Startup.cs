11b1b1f1d4cf91b4003a5299fcf2aa926b93fa4e
Ivan Frolov <mail.f1van@gmail.com>
2023-10-22 19:09:17 +0300

    Added code in Startup.cs for linking main modules with each other to organize gameplay

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -5,12 +5,33 @@ public class Startup : MonoBehaviour
 {
     [SerializeField] private MissionsContainerSO _missions;
     [SerializeField] private Map _map;
+    [SerializeField] private MissionUI _ui;
+    [SerializeField] private PlayerInputsService _inputsService;
     
     private CampaignProgression _campaignProgression;
+    private MissionsStorage _missionsStorage;
+    private HeroesStorage _heroesStorage;
 
     void Awake()
     {
-        _campaignProgression = new CampaignProgression(_missions);
-        _map.Initialize(_campaignProgression.MissionsContainer);
+        _inputsService.Initialize();
+        
+        _missionsStorage = new MissionsStorage(_missions);
+        _heroesStorage = new HeroesStorage();
+        _campaignProgression = new CampaignProgression(_missionsStorage, _heroesStorage);
+        
+        _map.Initialize(_missionsStorage);
+        
+        _map.SelectedMission += _missionsStorage.SelectMission;
+        _missionsStorage.MissionSelected += _ui.ShowMissionPreview;
+        
+        _ui.MissionStarted += _missionsStorage.StartSelectedMission;
+        _missionsStorage.MissionStarted += _ => _inputsService.DisableInputs();
+        _missionsStorage.MissionStarted += _ui.ShowMissionAccomplishment;
+        
+        _ui.MissionCompleted += _missionsStorage.CompleteStartedMission;
+        _missionsStorage.MissionCompleted += (_, __) => _inputsService.EnableInputs();
+        _missionsStorage.MissionCompleted += _campaignProgression.CompleteMission;
+        _missionsStorage.MissionCompleted += (_, __) => _ui.HideMission();
     }
 }
